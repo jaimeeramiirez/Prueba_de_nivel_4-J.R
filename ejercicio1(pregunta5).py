3364a78d41f6709d034b131026c1bc1b68dbdf12
jaimeeramiirez <jaime.ramirezrodriguez@usp.ceu.es>
2023-05-17 20:10:40 +0200

    Update ejercicio1(pregunta5).py

--- ejercicio1(pregunta5).py
+++ ejercicio1(pregunta5).py
@@ -2,7 +2,6 @@ import numpy as np # importamos numpy
 from heapq import heappop, heappush # importamos heappop y heappush de heapq para la cola de prioridad (heap)
 
 # Definición de personajes y grafo
-personajes = ['Iron Man', 'The Incredible Hulk', 'Khan', 'Thor', 'Captain America', 'Ant-Man', 'Nick Fury', 'The Winter Soldier']
 grafo = np.array([
   [0, 6, 0, 1, 8, 7, 3, 2],
   [6, 0, 0, 6, 1, 8, 9, 1],
@@ -14,8 +13,11 @@ grafo = np.array([
   [2, 1, 0, 3, 5, 6, 1, 0]
 ]) # matriz de adyacencia
 
-# Algoritmo de Prim
-def prim(grafo, inicio): # inicio es el índice del nodo inicial
+superheroes = ['Iron Man', 'The Incredible Hulk', 'Khan', 'Thor', 'Captain America', 'Ant-Man', 'Nick Fury', 'The Winter Soldier']
+
+
+# -----------------------------
+def algoritmo(grafo, inicio): # inicio es el índice del nodo inicial
     n = len(grafo) # número de nodos
     visitados = [False]*n # lista de nodos visitados
     pesos = [-1]*n # lista de pesos de los nodos
@@ -38,12 +40,12 @@ def prim(grafo, inicio): # inicio es el índice del nodo inicial
 # APARTADO B
 # -----------------------------
 print("\n---APARTADO B---") 
-inicio = personajes.index('Iron Man') # índice del nodo inicial
-previos = prim(grafo, inicio) # lista de nodos previos
+inicio = superheroes.index('Iron Man') # índice del nodo inicial
+previos = algoritmo(grafo, inicio) # lista de nodos previos
 print("Árbol de expansión máximo desde Iron Man:") 
 for i, p in enumerate(previos): # para cada nodo
     if p is not None: # si tiene nodo previo
-        print(f'{personajes[p]} -- {personajes[i]}') # mostramos el nodo previo y el nodo
+        print(f'{superheroes[p]} -- {superheroes[i]}') # mostramos el nodo previo y el nodo
 
 # -----------------------------
 # APARTADO C
@@ -51,26 +53,26 @@ for i, p in enumerate(previos): # para cada nodo
 print("\n---APARTADO C---")
 max_episodios = np.max(grafo) # máximo número de episodios compartidos
 print(f'\nMáximo número de episodios compartidos: {max_episodios}') 
-print('Pares de personajes que comparten el máximo número de episodios:') 
-indices = np.where(grafo == max_episodios) # índices de los pares de personajes que comparten el máximo número de episodios
-for i in range(len(indices[0])): # para cada par de personajes
+print('Pares de superheroes que comparten el máximo número de episodios:') 
+indices = np.where(grafo == max_episodios) # índices de los pares de superheroes que comparten el máximo número de episodios
+for i in range(len(indices[0])): # para cada par de superheroes
     if indices[0][i] < indices[1][i]: # si el primer índice es menor que el segundo
-        print(f'{personajes[indices[0][i]]} -- {personajes[indices[1][i]]}') # mostramos el par de personajes
+        print(f'{superheroes[indices[0][i]]} -- {superheroes[indices[1][i]]}') # mostramos el par de superheroes
 
 # -----------------------------
 # APARTADO D
 # -----------------------------
 print("\n---APARTADO D---") 
-print('\nTodos los personajes:') 
-for personaje in personajes: # para cada personaje
+print('\nTodos los superheroes:') 
+for personaje in superheroes: # para cada personaje
     print(personaje) # mostramos el personaje
 
 # -----------------------------
 # APARTADO E
 # -----------------------------
 print("\n---APARTADO E---")
-print('\nPersonajes que aparecieron en nueve episodios de la saga:')
-indices_nueve_episodios = np.where(grafo == 9) # índices de los pares de personajes que comparten nueve episodios
-for i in range(len(indices_nueve_episodios[0])): # para cada par de personajes
+print('\superheroes que aparecieron en nueve episodios de la saga:')
+indices_nueve_episodios = np.where(grafo == 9) # índices de los pares de superheroes que comparten nueve episodios
+for i in range(len(indices_nueve_episodios[0])): # para cada par de superheroes
     if indices_nueve_episodios[0][i] < indices_nueve_episodios[1][i]: # si el primer índice es menor que el segundo
-        print(f'{personajes[indices_nueve_episodios[0][i]]} -- {personajes[indices_nueve_episodios[1][i]]}') # mostramos el par de personajes
+        print(f'{superheroes[indices_nueve_episodios[0][i]]} -- {superheroes[indices_nueve_episodios[1][i]]}') # mostramos el par de personajes
